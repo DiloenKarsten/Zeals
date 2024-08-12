e6b2fa2c7feff37378ca29134282707bb2007ac3
Stefan Vejrum <73114791+DiloenKarsten@users.noreply.github.com>
2024-08-12 11:49:16 +0200

    SpawnManager spawn enemies
    I made a spawn manager which uses a list to keep track of the different enemy types and which to spawn. It will require a rewrite when i introduce new enemy types.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public List<Enemy> enemyPrefabs;
+    public GameObject gameArea;
+    private int enemyCount;
+    private float spawnRange;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnRange = gameArea.GetComponent<BoxCollider>().size.x*2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount<3)
+        {
+            Instantiate(enemyPrefabs[0], GenerateSpawnPoint(), enemyPrefabs[0].transform.rotation);
+        }
         
+    }
+
+    private Vector3 GenerateSpawnPoint()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 1.5f, spawnPosZ);
+        return randomPos;
     }
 }
